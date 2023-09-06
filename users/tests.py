86bdb944112f009ccaadc4c32dd08ec778177d5d
Серж Рыж (playginger) <7playginger7@gmail.com>
2023-09-07 01:44:50 +0300

    DZ 25.2

--- users/tests.py
+++ users/tests.py
@@ -1,41 +1,46 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 from .models import Lesson, Well, Subscription
 from django.contrib.auth.models import User
 
 
 class LessonCRUDTest(TestCase):
     def setUp(self):
-        self.well = Well.objects.create(title='Test Well')
+        self.course = Well.objects.create(title='Test Course')
 
     def test_create_lesson(self):
-        lesson = Lesson.objects.create(title='Test Lesson', description=self.well)
-        self.assertEqual(lesson.title, 'Test Lesson')
+        response = self.client.post('/api/lessons/', {'title': 'Test Lesson', 'course': self.course.id})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Lesson.objects.count(), 1)
+        self.assertEqual(Lesson.objects.last().title, 'Test Lesson')
 
     def test_update_lesson(self):
-        lesson = Lesson.objects.create(title='Test Lesson', description=self.well)
-        lesson.title = 'Updated Lesson'
-        lesson.save()
-        self.assertEqual(lesson.title, 'Updated Lesson')
+        lesson = Lesson.objects.create(title='Test Lesson', description=self.course)
+        response = self.client.put(f'/api/lessons/{lesson.id}/', {'title': 'Updated Lesson'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Lesson.objects.get(id=lesson.id).title, 'Updated Lesson')
 
     def test_delete_lesson(self):
-        lesson = Lesson.objects.create(title='Test Lesson', description=self.well)
-        lesson.delete()
-        self.assertFalse(Lesson.objects.filter(title='Test Lesson').exists())
+        lesson = Lesson.objects.create(title='Test Lesson', description=self.course)
+        response = self.client.delete(f'/api/lessons/{lesson.id}/')
+        self.assertEqual(response.status_code, 204)
+        self.assertFalse(Lesson.objects.filter(id=lesson.id).exists())
 
 
 class SubscriptionTest(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(user='testuser')
-        self.course = Well.objects.create(title='Test Well')
+        self.user = User.objects.create_user(username='testuser')
+        self.course = Well.objects.create(title='Test Course')
 
     def test_subscribe_to_course(self):
-        subscription = Subscription.objects.create(user=self.user, course=self.course, subscribed=True)
-        self.assertTrue(subscription.subscribed)
+        self.client.force_login(self.user)
+        response = self.client.post(f'/api/courses/{self.course.id}/subscribe/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(Subscription.objects.filter(user=self.user, course=self.course, subscribed=True).exists())
 
     def test_unsubscribe_to_course(self):
         subscription = Subscription.objects.create(user=self.user, course=self.course, subscribed=True)
-        subscription.subscribed = False
-        subscription.save()
-        self.assertFalse(subscription.subscribed)
-
-
+        self.client.force_login(self.user)
+        response = self.client.post(f'/api/courses/{self.course.id}/unsubscribe/')
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(Subscription.objects.filter(id=subscription.id, subscribed=True).exists())
