79b592a78f32a5dcd91b8ff472a9bc8ae7235587
Серж Рыж (playginger) <7playginger7@gmail.com>
2023-09-09 22:38:14 +0300

    DZ 25.2.3

--- users/tests.py
+++ users/tests.py
@@ -25,27 +25,29 @@ class SubscriptionTest(TestCase):
     def test_create_lesson(self):
         self.client.force_authenticate(self.user)
         response = self.client.get('/api/lessons/')
-        self.assertEqual(response.status_code, 200)
-        # self.assertEqual(Lesson.objects.count(), 1)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Lesson.objects.count(), 1)
         # self.assertEqual(Lesson.objects.last().title, 'Test Lesson')
 
     def test_update_lesson(self):
         self.client.force_authenticate(self.user)
         lesson = Lesson.objects.create(title='Test Lesson', description=self.course)
-        response = self.client.get('/api/lessons/')
+        response = self.client.put(f'/api/lessons/{lesson.id}/', {'title': 'Updated Lesson'})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(Lesson.objects.get(id=lesson.id).title, 'Test Lesson' )
+        self.assertEqual(Lesson.objects.get(id=lesson.id).title, 'Test Lesson')
 
     def test_delete_lesson(self):
         self.client.force_authenticate(self.user)
         lesson = Lesson.objects.create(title='Test Lesson', description=self.course)
-        response = self.client.get('/api/lessons/')
-        self.assertEqual(response.status_code, 200)
+        response = self.client.delete(f'/api/lessons/{lesson.id}/')
+        self.assertEqual(response.status_code, 204)
         # self.assertFalse(Lesson.objects.filter(id=lesson.id).exists())
 
     def test_subscribe_to_course(self):
         self.client.force_authenticate(self.user)
-        response = self.client.get('/api/lessons/')
+        response = self.client.post(
+            f'/api/lessons/{self.course.id}/subscribe/'
+        )
         self.assertEqual(response.status_code, 200)
         # self.assertTrue(Subscription.objects.filter(user=self.user, course=self.course, subscribed=True).exists())
 
@@ -53,6 +55,8 @@ class SubscriptionTest(TestCase):
         self.client.force_authenticate(self.user)
         subscription = Subscription.objects.create(user=self.user, course=self.course, subscribed=True)
         self.client.force_login(self.user)
-        response = self.client.get('/api/lessons/')
+        response = self.client.post(
+            f'/api/lessons/{self.course.id}/unsubscribe/'
+        )
         self.assertEqual(response.status_code, 200)
         # self.assertFalse(Subscription.objects.filter(id=subscription.id, subscribed=True).exists())
